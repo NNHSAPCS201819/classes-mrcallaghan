4415db5e3ea9e96b9dac3e33948e454a7c88f0cc
mattcallaghan <mcallaghan@naperville203.org>
2018-09-11 15:35:17 -0500

    Started Ch. 3 Notes - Car Class

---
+++ Class Notes/Car.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car
+{
+    /*
+     * Instance variable store the object's properties/attributes
+     *      specify the visibility (e.g. private)
+     *          public: accessible by anyone
+     *          private: only accessed through methods in the class
+     *      specify the type (e.g. double)
+     *      specify the name (e.g. fuelEfficiency)
+     *      
+     *  Instance variables differ from local variable in the following ways:
+     *      scoped to their class (accessible using the methods of the class)
+     *      automatically initialized to a default value (0, false, null)
+     */
+
+    private double fuelEfficiency;  // in units of miles per gallon
+    private double fuelInTank;      // in units of gallons
+
+    
+    
+    /*
+     * When defining a method, specify:
+     *      the visibility (e.g. public)
+     *      the return type (e.g. void)
+     *      the method name (e.g. drive)
+     *      the parameters and their types (e.g. distance of type double)
+     */
+    
+    /**
+     * Drives the car the specified distance, in miles, consuming fuel
+     * 
+     * @param distance the distance, in miles, this car drives
+     */
+}
