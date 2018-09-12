c67a728878ad1a6858a9f252cdc85ca95d64307b
mattcallaghan <mcallaghan@naperville203.org>
2018-09-12 15:16:26 -0500

    Added to Ch. 3 Notes
    Set up Car class and started to set up test class

--- Class Notes/Car.java
+++ Class Notes/Car.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Car here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author mcallaghan
+ * @version 11 September 2018
  */
 public class Car
 {
@@ -22,9 +22,37 @@ public class Car
 
     private double fuelEfficiency;  // in units of miles per gallon
     private double fuelInTank;      // in units of gallons
+    private String licensePlate;
+    
+    /*
+     * Constructor
+     *      responsible for initializing newly created objects
+     *      invoked automatically via the new operator
+     *      the name of the constructor must match the name of the class
+     *      has no return type (not even void)
+     *      multiple constructors may be defined for a class
+     *      one constructor may call another constructor (with restrictions)
+     */
+    
+    /**
+     * Default constructor for the class Car
+     *      Initialize the fuel efficiency to 30 mpg and the hfuel in teh car's tank to 0 gallons
+     */
+    public Car()
+    {
+        
+    }
+    
+    /**
+     * Constructs a new Car object with the specified fuel efficiency
+     * 
+     * @param initialEfficiency the initial fuel efficiency in mpg, of this new car
+     */
+    public Car( double initialEfficiency )
+    {
+        
+    }
 
-    
-    
     /*
      * When defining a method, specify:
      *      the visibility (e.g. public)
@@ -38,4 +66,53 @@ public class Car
      * 
      * @param distance the distance, in miles, this car drives
      */
+    public void drive( double distance )
+    {
+        
+    }
+    
+    /**
+     * Adds the specified amount of fuel to this car's tank
+     * 
+     * @param amount the amount of fuel, in gallons, to add to the car's tank
+     */
+    public void addFuel( double amount )
+    {
+        
+    }
+    
+    /**
+     * Returns the amount of fuel in this car's tank
+     * 
+     * @return the amount of fuel, in galons, in this car's tank
+     */
+    public double getFuelInTank()
+    {
+        return 0;
+    }
+    
+    /**
+     * Sets the license plate to the specified value
+     * 
+     * @param plate the license plate for this car
+     */
+    public void setLicensePlate( String plate )
+    {
+        
+    }
+    
+    /**
+     * Gets the license plate of this car
+     * 
+     * @return the license plate of this car
+     */
+    public String getLicensePlate()
+    {
+        return "";
+    }
+    
+    
+    
+    
+    
 }
